93797c7e6ab8b740e6fea8fea525f8c2801da82e
Brian Romaniuk <brirom@telus.net>
2021-09-11 14:45:50 -0700

    Create base react app

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom'
+import 'Navbar.css';
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            
+        </div >
+    )
+};
+
+export default Navbar;
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'react-router-dom'
+import './Home.css';
+import '../../App.css';
+
+function Home() {
+    return (
+        <div className='home'>
+            
+        </div >
+    )
+};
+
+export default Home;
